5bb3a3013125c171cb856b4d7d343724aa67ce7e
bowsky <bowsky@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-01-29 16:45:34 +0000

    1/29/08 - Updated website to new style
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/prob@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,59 +1,56 @@
-<!-- This is the project specific website template -->
-<!-- It can be changed as liked or replaced by other content -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
 
+
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"><title>PROB - Probability on Finite Sample Spaces</title>
+	
+	<link rel="Shortcut Icon" href="http://ipsur.r-forge.r-project.org/favicon.ico">
+
+	<style type="text/css">
+	<!--
+		@import url("main.css");
+		
+
+	-->
+	</style></head><body>
+	<div align="center">
+	
+	<table border="0" cellpadding="0" cellspacing="0" height="100%" width="700">
+  		<tbody><tr>
+    		<td align="left" bgcolor="#ffffff" valign="top">
+			<!-- All of the page content is in this table-->
+
+<!--BEGIN: Page Banner-->
 <?php
-
-$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
-$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
-$themeroot='http://r-forge.r-project.org/themes/rforge/';
-
-echo '<?xml version="1.0" encoding="UTF-8"?>';
+	include("banner.php");
 ?>
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
+<!--END: Page Banner-->
+	
+<!--BEGIN: Top Menu-->
+<?php
+	//include("topMenu.php");
+?>
+<!--END: Top Menu-->
 
-  <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>The prob Package</title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
-    <style type="text/css">
-<!--
-.style1 {
-	font-family: "Courier New", Courier, monospace;
-	font-size: 18px;
-}
--->
-    </style>
-</head>
+	
+      <p class="articleTitle">What PROB Does:</p>
+      <blockquote>
+        <p>This package is for performing elementary probability on finite  sample spaces, represented by data frames or lists. Functionality  includes setting up sample spaces, set algebra, probability and  conditional probability, tools for simulation, and more.
+          <!-- end of project description -->
+        </p>
+        <p>Details and examples can be found in the package vignette, which is copied <a href="doc/prob.pdf">here</a>.</p>
+        <p>The CRAN description is <a href="http://www.cran.r-project.org/src/contrib/Descriptions/prob.html">here</a>.</p>
+      </blockquote>
+      <p class="articleTitle">Contact Information </p>
+      <blockquote>
+        <p> <span class="name">PROB</span> comments, suggestions, bugs:   <a href="mailto:prob-help@list.r-forge.org">prob-help@list.r-forge.org</a><br>
+G. Jay Kerns: <a href="mailto:gkerns@ysu.edu">gkerns@ysu.edu</a> (<a href="http://www.cc.ysu.edu/%7Egjkerns">home page</a>)</p>
+        <p>&nbsp;</p>
+      </blockquote>
+	  <p align="center">
+	  (C) R Foundation, from <a href="http://www.r-project.org">http://www.r-project.org</a>	  </p></td>
+  </tr>
+</tbody></table>
 
-<body>
-
-<! --- R-Forge Logo --- >
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tr><td>
-<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
-</table>
-
-
-<!-- get project title  -->
-<!-- own website starts here, the following may be changed as you like -->
-
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
-
-<!-- end of project description -->
-
-<p>&nbsp;</p>
-<p>More content  is coming soon. </p>
-<p>Details and examples can be found in the package vignette, which is copied <a href="doc/prob.pdf"><strong>here</strong></a>.</p>
-<p>The CRAN description is <a href="http://www.cran.r-project.org/src/contrib/Descriptions/prob.html"><strong>here</strong></a>. </p>
-<p>The <span class="style1">R</span>-Forge <strong>project summary page</strong> is <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
-</body>
-</html>
+</div>
+</body></html>
