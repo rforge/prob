149c4d2ff04b703a13598e955d48c4d0aa3b21a4
bowsky <bowsky@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-01-29 16:49:32 +0000

    1/29/08 - Updated website to new style (All files now uploaded)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/prob@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/banner.php
@@ -0,0 +1,16 @@
+<table border="0" cellpadding="0" cellspacing="0" height="140" width="700">
+	<tbody><tr>
+    	<td align="center" valign="middle" width="200">
+			<a href="http://www.r-project.org/index.php">
+				<img src="resources/banner.corner.jpg" alt="R" border="0" height="100" width="200">
+			</a>
+			Hosted by <a href="https://r-forge.r-project.org/"><font color="#000000">R-Forge</font></a>	
+		</td>
+        <td align="center" height="120" valign="middle" width="500">
+        	<p>
+				<a href="http://prob.r-forge.r-project.org/index.php">
+					<img src="resources/banner.jpg" alt="Introduction to Probability and Statistics Using R" border="0" height="100" width="500">				</a>
+			G. Jay Kerns, Youngstown State University</p>
+	  </td>
+  	</tr>
+</tbody></table>
---
+++ www/topmenu.php
@@ -0,0 +1,16 @@
+<div id="navMenu">
+  <table border="0" cellpadding="0" cellspacing="0" width="700">
+    <tbody><tr>
+      <td align="center" valign="middle" width="175"><div align="center"><a href="http://prob.r-forge.r-project.org/index.php">Home</a></div></td>
+      <td align="center" valign="middle" width="175"><div align="center"><a href="http://prob.r-forge.r-project.org/description.php">Description</a></div></td>
+      <td align="center" valign="middle" width="175"><div align="center"><a href="http://prob.r-forge.r-project.org/features.php">Features</a></div></td>
+	  <td align="center" valign="middle" width="175"><div align="center"><a href="http://prob.r-forge.r-project.org/news.php">What's New </a></div></td>
+    </tr>
+    <tr>
+      <td align="center" valign="middle" width="175"><div align="center"><a href="http://prob.r-forge.r-project.org/installation.php">Installation</a></div></td>
+      <td align="center" valign="middle" width="175"><div align="center"><a href="http://prob.r-forge.r-project.org/downloads.php">Downloads</a></div></td>
+      <td align="center" valign="middle" width="175"><div align="center"><a href="http://prib.r-forge.r-project.org/screens.php">Screen Shots </a></div></td>
+	  <td align="center" valign="middle" width="175"><div align="center"><a href="http://prob.r-forge.r-project.org/links.php">Links</a></div></td>
+    </tr>
+  </tbody></table>
+</div>
