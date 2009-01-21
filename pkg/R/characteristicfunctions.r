fc140ed1eec982346c34ee4baa01ac002c5cb3c0
gkerns <gkerns@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-01-21 19:28:55 +0000

    changed names to cf***, added noncentral t and beta
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/prob@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/characteristicfunctions.r
+++ pkg/R/characteristicfunctions.r
@@ -3,14 +3,23 @@
 #  Copyright January 2009, G. Jay Kerns
 
 
-cbeta <- function(t, shape1, shape2){
+cfbeta <- function(t, shape1, shape2, ncp = 0){
     if (shape1 <=0 || shape2 <=0)
         stop("shape1, shape2 must be positive")
-    fAsianOptions:::kummerM(1i*t, shape1, shape1 + shape2)
+    if (identical(all.equal(ncp, 0), TRUE)){
+        require(fAsianOptions)
+        kummerM(1i*t, shape1, shape1 + shape2)
+    } else {
+        fr <- function(x) cos(t*x)*dbeta(x, shape1, shape2, ncp)
+        fi <- function(x) sin(t*x)*dbeta(x, shape1, shape2, ncp)
+        Rp <- integrate(fr, lower = 0, upper = 1)$value
+        Ip <- integrate(fi, lower = 0, upper = 1)$value
+        return( Rp + 1i*Ip )
+    }
 }
 
 
-cbinom <- function(t, size, prob){
+cfbinom <- function(t, size, prob){
     if (size <= 0 )
         stop("size must be positive")
     if (prob < 0 || prob > 1)
@@ -19,53 +28,54 @@ cbinom <- function(t, size, prob){
 }
 
 
-ccauchy = function(t, location = 0, scale = 1){
+cfcauchy = function(t, location = 0, scale = 1){
     if (scale <= 0 )
         stop("scale must be positive")
     exp(1i*location*t - scale*abs(t))  
 }
 
-cchisq <- function(t, df, ncp = 0){
+cfchisq <- function(t, df, ncp = 0){
     if (df < 0 || ncp < 0  )
         stop("df and ncp must be nonnegative")
     exp(1i*ncp*t/(1-2i*t))/(1 - 2i*t)^(df/2)
 }
 
-cexp <- function(t, rate = 1){
-    cgamma(t, shape = 1, scale = 1/rate)
+cfexp <- function(t, rate = 1){
+    cfgamma(t, shape = 1, scale = 1/rate)
 }
 
-cf <- function(t, df1, df2, ncp = 0, kmax = 10){
+cff <- function(t, df1, df2, ncp, kmax = 10){
     if (df1 <= 0 || df2 <= 0  )
         stop("df1 and df2 must be positive")
+    require(fAsianOptions)
     if( identical(all.equal(ncp, 0), TRUE) ){
-        gamma((df1+df2)/2) / gamma(df2/2) * fAsianOptions:::kummerU(-1i*df2*t/df1, df1/2, 1 - df2/2)
+        gamma((df1+df2)/2) / gamma(df2/2) * kummerU(-1i*df2*t/df1, df1/2, 1 - df2/2)
     } else {
-        exp(-ncp/2)*sum((ncp/2)^(0:kmax)/factorial(0:kmax)* fAsianOptions:::kummerM(-1i*df2*t/df1, df1/2 + 0:kmax, -df2/2))
+        exp(-ncp/2)*sum((ncp/2)^(0:kmax)/factorial(0:kmax)* kummerM(-1i*df2*t/df1, df1/2 + 0:kmax, -df2/2))
     }
 }
 
 
-cgamma <- function(t, shape, rate = 1, scale = 1/rate){
+cfgamma <- function(t, shape, rate = 1, scale = 1/rate){
     if (rate <= 0  || scale <= 0)
         stop("rate must be positive")
     (1-scale*1i*t)^(-shape)
 }
 
 
-cgeom <- function(t, prob){
-    cnbinom(t, size = 1, prob = prob)
+cfgeom <- function(t, prob){
+    cfnbinom(t, size = 1, prob = prob)
 }
 
 
-chyper <- function(t, m, n, k){
+cfhyper <- function(t, m, n, k){
     if (m < 0 || n < 0 || k < 0)
         stop("m, n, k must be positive")
     hypergeo:::hypergeo(-k, -m, n - k + 1, exp(1i*t))/hypergeo:::hypergeo(-k, -m, n - k + 1, 1)
 }
 
 
-clnorm <- function(t, meanlog = 0, sdlog = 1){
+cflnorm <- function(t, meanlog = 0, sdlog = 1){
     if (sdlog <= 0)
         stop("sdlog must be positive")
     if (identical(all.equal(t, 0), TRUE)){
@@ -81,7 +91,7 @@ clnorm <- function(t, meanlog = 0, sdlog = 1){
 }
 
 
-clogis <- function(t, location = 0, scale = 1){
+cflogis <- function(t, location = 0, scale = 1){
     if (scale <= 0)
         stop("scale must be positive")
     ifelse( identical(all.equal(t, 0), TRUE),
@@ -90,7 +100,7 @@ clogis <- function(t, location = 0, scale = 1){
 }
 
 
-cnbinom <- function(t, size, prob, mu){
+cfnbinom <- function(t, size, prob, mu){
     if (size <= 0 )
         stop("size must be positive")
     if (prob <= 0 || prob > 1)
@@ -104,27 +114,35 @@ cnbinom <- function(t, size, prob, mu){
 }
 
 
-cnorm <- function(t, mean = 0, sd = 1){
+cfnorm <- function(t, mean = 0, sd = 1){
     if (sd <= 0)
         stop("sd must be positive")
     exp(1i*mean - (sd*t)^2/2)  
 }
 
-cpois <- function(t, lambda){
+cfpois <- function(t, lambda){
     if (lambda <= 0)
         stop("lambda must be positive")
     exp(lambda*(exp(1i*t) - 1))
 }
 
 
-ct <- function(t, df){
+cft <- function(t, df, ncp){
     if (df <= 0)
-        stop("df be positive")
-    besselK(sqrt(df)*abs(t), df/2)*(sqrt(df)*abs(t))^(df/2)/( gamma(df/2) * 2^(df/2 - 1) )
+        stop("df must be positive")
+    if (identical(all.equal(ncp, 0), TRUE)){
+        as.complex(besselK(sqrt(df)*abs(t), df/2)*(sqrt(df)*abs(t))^(df/2)/( gamma(df/2) * 2^(df/2 - 1) ))
+    } else {
+        fr <- function(x) cos(t*x)*dt(x, df, ncp)
+        fi <- function(x) sin(t*x)*dt(x, df, ncp)
+        Rp <- integrate(fr, lower = -Inf, upper = 0)$value
+        Ip <- integrate(fi, lower = -Inf, upper = 0)$value
+        return(Rp + 1i*Ip)
+    }
 }
 
 
-cunif <- function(t, min = 0, max = 1){
+cfunif <- function(t, min = 0, max = 1){
     if (max < min)
         stop("min cannot be greater than max")
     ifelse( identical(all.equal(t, 0), TRUE),
@@ -133,7 +151,7 @@ cunif <- function(t, min = 0, max = 1){
 }
 
 
-cweibull <- function(t, shape, scale = 1, kmax = 20){
+cfweibull <- function(t, shape, scale = 1, kmax = 20){
     if (shape <= 0 || scale <= 0)
         stop("shape and scale must be positive")
     1 + sum((1i*t)^(0:kmax+1)/factorial(0:kmax) * scale^(0:kmax+1)/shape * gamma((0:kmax+1)/scale) )
